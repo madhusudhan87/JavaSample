c74ab4fb8d0895a644517d1b0cf29368bdb37b4c
satwikraman <45010392+satwikraman@users.noreply.github.com>
2019-02-06 14:32:08 -0800

    this is modified

--- JavaSample/src/JavaSample.java
+++ JavaSample/src/JavaSample.java
@@ -2,7 +2,7 @@
 public class JavaSample {
 
 	public static void main(String[] args) {
-		System.out.println("hii how ru i am from eclipse");
+		System.out.println("hii how ru i am frome");
 	}
 
 }
